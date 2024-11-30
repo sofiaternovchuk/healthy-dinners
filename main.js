8390840375c1a163c7ae82358b36c6a3d9a4cbe6
sofiaternovchuk <sofia090706@gmail.com>
2024-11-30 11:08:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ async function getResponce() {
     let content = await responce.text()
     console.log(content)
     content = JSON.parse(content)
-    content = content.splice(0, 6)
+    content = content.splice(0, 5)
     console.log(content)
 
     let key
